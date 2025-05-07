bd536bfcff149f09216a6eb7c7d4ca6abdacf2d9
Valentino <valentinoof.dev@gmail.com>
2025-05-07 11:37:37 -0500

    fix:Migrado a Docker, ya no es necesario usar XAMPP

--- my-app/run.py
+++ my-app/run.py
@@ -1,12 +1,7 @@
-# Declarando nombre de la aplicación e inicializando, crear la aplicación Flask
 from app import app
-
-# Importando todos mis Routers (Rutas)
 from routers.router_login import *
 from routers.router_home import *
 from routers.router_page_not_found import *
 
-
-# Ejecutando el objeto Flask
 if __name__ == '__main__':
-    app.run(debug=True, port=5600)
+    app.run(debug=True, host='0.0.0.0', port=5000)
